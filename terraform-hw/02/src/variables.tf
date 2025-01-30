a59be9139f2289c39a87617d254a6ffdb3107f0a
Daimero88 <ussr88@mail.ru>
2025-01-30 21:15:34 +0300

    update files

--- terraform-hw/02/src/variables.tf
+++ terraform-hw/02/src/variables.tf
@@ -1,14 +1,15 @@
 ###cloud vars
 
-
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default = "b1gutnkqmso25ie96r20"
 }
 
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default = "b1gq6j5ndke5qjs67m85"
 }
 
 variable "default_zone" {
@@ -16,9 +17,22 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
+variable "b-zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "b-zone_cidr" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
@@ -28,11 +42,19 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "vpc_db" {
+  type        = string
+  default     = "db"
+  description = "VPC network & subnet db"
+}
 
 ###ssh vars
 
-variable "vms_ssh_root_key" {
-  type        = string
-  default     = "<your_ssh_ed25519_key>"
-  description = "ssh-keygen -t ed25519"
-}
+#variable "vms_ssh_root_key" {
+#  type        = string
+#  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHiZjyQiMYyn9ZJrggVSncksTiG+2NMFf2TVZmROmb6O ssilchin-deb"
+#  description = "ssh-keygen -t ed25519"
+#}
+
+#variable "ssh_key" {
+#}
---
+++ terraform-hw/02/src/vms_platform.tf
@@ -0,0 +1,84 @@
+variable "vm_web_image" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "image"
+}
+
+variable "vm_web_user" {
+  type        = string
+  default     = "ubuntu"
+  description = "user"
+}
+
+variable "vm_web_name" {
+  type        = string
+  description = "Name"
+  default     = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  description = "platform ID"
+  default     = "standard-v3"
+}
+
+#variable "vm_web_cores" {
+#  type        = number
+#  description = "CPU"
+#  default     = 2
+#}
+
+#variable "vm_web_memory" {
+#  type        = number
+#  description = "RAM"
+#  default     = 1
+#}
+
+#variable "vm_web_core_fraction" {
+#  type        = number
+#  description = "CPU%"
+#  default     = 20
+#}
+
+variable "vm_db_name" {
+  type        = string
+  description = "Name"
+  default     = "netology-develop-platform-db"
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  description = "platform ID"
+  default     = "standard-v3"
+}
+
+#variable "vm_db_cores" {
+#  type        = number
+#  description = "CPU"
+#  default     = 2
+#}
+
+#variable "vm_db_memory" {
+#  type        = number
+#  description = "RAM"
+#  default     = 2
+#}
+
+#variable "vm_db_core_fraction" {
+#  type        = number
+#  description = "CPU%"
+#  default     = 20
+#}
+
+variable "vms_resources" {
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+  }))
+}
+
+variable "metadata" {
+  type = map(string)
+  description = "Metadata"
+}

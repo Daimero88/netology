84ab507fc1d0700f73f3e46664b221f5801b9aa6
Daimero88 <ussr88@mail.ru>
2025-02-04 21:33:10 +0300

    update main

--- terraform-hw/04/src/main.tf
+++ terraform-hw/04/src/main.tf
@@ -1,10 +1,72 @@
+#создаем облачную сеть
 resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+  name = "develop"
 }
 
+#создаем подсеть
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "develop-ru-central1-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.1.0/24"]
+}
+
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "develop-ru-central1-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.2.0/24"]
+}
+
+
+module "marketing-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a","ru-central1-b"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
+  instance_name  = "marketing-vm"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = {
+    project = "marketing"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_name  = "analytics-vm"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = {
+    project = "analytics"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars     = {
+    ssh-authorized-keys = file(var.ssh-authorized-keys[0])
+  }
+}

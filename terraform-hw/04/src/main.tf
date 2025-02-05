66ba70aeb7cf03c34cc02ee79589b8e5ac963e1a
Daimero88 <ussr88@mail.ru>
2025-02-05 13:22:09 +0300

    update

--- terraform-hw/04/src/main.tf
+++ terraform-hw/04/src/main.tf
@@ -4,27 +4,26 @@ resource "yandex_vpc_network" "develop" {
 }
 
 #создаем подсеть
-resource "yandex_vpc_subnet" "develop_a" {
-  name           = "develop-ru-central1-a"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
-}
+#resource "yandex_vpc_subnet" "develop_a" {
+#  name           = "develop-ru-central1-a"
+#  zone           = "ru-central1-a"
+#  network_id     = yandex_vpc_network.develop.id
+#  v4_cidr_blocks = ["10.0.1.0/24"]
+#}
 
-resource "yandex_vpc_subnet" "develop_b" {
-  name           = "develop-ru-central1-b"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.2.0/24"]
-}
-
+#resource "yandex_vpc_subnet" "develop_b" {
+#  name           = "develop-ru-central1-b"
+#  zone           = "ru-central1-b"
+#  network_id     = yandex_vpc_network.develop.id
+#  v4_cidr_blocks = ["10.0.2.0/24"]
+#}
 
 module "marketing-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "develop" 
-  network_id     = yandex_vpc_network.develop.id
-  subnet_zones   = ["ru-central1-a","ru-central1-b"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [module.vpc_dev.subnet_id]
   instance_name  = "marketing-vm"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
@@ -44,9 +43,9 @@ module "marketing-vm" {
 module "analytics-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "stage"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  subnet_ids     = [module.vpc_dev.subnet_id]
   instance_name  = "analytics-vm"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"

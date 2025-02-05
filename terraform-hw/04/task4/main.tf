9959dbe857f9012235123ecd2965f5ebc4bcb3e4
Daimero88 <ussr88@mail.ru>
2025-02-05 17:31:54 +0300

    add task4

---
+++ terraform-hw/04/task4/main.tf
@@ -0,0 +1,69 @@
+module "vpc_dev" {
+  source   = "./vpc"
+  env_name = "develop"
+  subnets  = [
+    { zone = "ru-central1-a",
+      cidr = "10.0.1.0/24" }
+    ]
+}
+
+module "vpc_prod" {
+  source   = "./vpc"
+  env_name = "production"
+  subnets  = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-d", cidr = "10.0.3.0/24" },
+  ]
+}
+
+module "marketing-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop"
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [module.vpc_dev.subnet_id]
+  instance_name  = "marketing-vm"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = {
+    project = "marketing"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [module.vpc_dev.subnet_id]
+  instance_name  = "analytics-vm"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = {
+    project = "analytics"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+
+}
+
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars     = {
+    ssh-authorized-keys = file(var.ssh-authorized-keys[0])
+  }
+}

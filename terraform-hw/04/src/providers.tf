29d2a5684cff85facc1ec18c4a75b62a05f0b7fa
Daimero88 <ussr88@mail.ru>
2025-02-07 20:19:48 +0300

    add providers.tf

--- terraform-hw/04/src/providers.tf
+++ terraform-hw/04/src/providers.tf
@@ -5,6 +5,33 @@ terraform {
     }
   }
   required_version = ">=1.8.4"
+  backend "s3" {
+    
+    shared_credentials_files = ["~/.aws/credentials"]
+    shared_config_files = [ "~/.aws/config" ]
+    profile = "default"
+    region="ru-central1"
+
+    bucket     = "ssilchin-s3"
+    key = "production/terraform.tfstate"
+    
+
+    # access_key                  = "..."          #Только для примера! Не хардкодим секретные данные!
+    # secret_key                  = "..."          #Только для примера! Не хардкодим секретные данные!
+
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  endpoints ={
+    dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gutnkqmso25ie96r20/etnikd5j238psduc733o"
+    s3 = "https://storage.yandexcloud.net"
+  }
+
+    dynamodb_table              = "tfstate-prod"
+  }
 }
 
 provider "yandex" {

9da5d0cd37c749b31550b268e0293cb2af893b14
Daimero88 <ussr88@mail.ru>
2025-04-14 12:53:15 +0300

    main.py

---
+++ monitoring-hw/04/main.py
@@ -0,0 +1,14 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://c171133eb197d3351aaa9f4e9be9574a@o4509150990368768.ingest.de.sentry.io/4509151004983376",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    environment="development",
+    release="1.0"
+)
+
+if __name__ == "__main__":
+    divizion_zero = 1 / 0
+

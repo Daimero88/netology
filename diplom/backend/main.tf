261b9b46d3252ff6bd2c98c5f19b1f636682c3d1
Daimero88 <ussr88@mail.ru>
2025-08-06 14:45:32 +0300

    change main.tf

--- diplom/backend/main.tf
+++ diplom/backend/main.tf
@@ -17,17 +17,4 @@ resource "yandex_storage_bucket" "terraform_state" {
   bucket     = "ssilchin-diplom"
   access_key = var.sa_access_key
   secret_key = var.sa_secret_key
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = null
-        sse_algorithm     = "AES256"
-      }
-    }
-  }
 }

1cc47e002a24fea1128bca55b22d557f6cd46cf9
Daimero88 <ussr88@mail.ru>
2025-08-14 15:55:22 +0300

    update test.tf

--- diplom/infrastructure/test.tf
+++ diplom/infrastructure/test.tf
@@ -1,8 +1,4 @@
-output "subnet_ids" {
-  value = {
-    "ru-central1-a" = yandex_vpc_subnet.subnet_a.id
-    "ru-central1-b" = yandex_vpc_subnet.subnet_b.id
-    "ru-central1-d" = yandex_vpc_subnet.subnet_d.id
-  }
-  description = "Map of subnet IDs by availability zone"
+output "atlantis_test" {
+  value = "atlantis test"
+  description = "just atlantis test"
 }

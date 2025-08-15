93c356d5ae917ff165be8b1058a58aaf27501f6f
Daimero88 <ussr88@mail.ru>
2025-08-15 20:34:13 +0300

    update ycr.tf

--- diplom/infrastructure/ycr.tf
+++ diplom/infrastructure/ycr.tf
@@ -2,3 +2,11 @@ resource "yandex_container_registry" "this" {
   name = var.ycr_name
 }
 
+resource "yandex_container_registry_iam_binding" "public_access" {
+  registry_id = yandex_container_registry.this.id
+  role        = "container-registry.viewer"
+
+  members = [
+    "system:allUsers"
+  ]
+}
